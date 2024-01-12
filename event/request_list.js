726c6147efdd417400a9403cc7d4e1143e3ca2d0
tisoz <admin@tisoz.com>
2024-01-12 10:24:01 +0800

    commit

--- event/request_list.js
+++ event/request_list.js
@@ -1,6 +1,6 @@
 let player_postion_send = require("./player_postion_send.js")
 let map_load = require("./map_load.js")
-let Join_map = require("./Join_map.js")
+let Join_map = require("./join_map.js")
 let request_list = {
     2: Join_map,
     21: player_postion_send,
